24ea3cd41933beb574c23888ab130dcbef89a2e8
Golubkov <chpokreacher@gmail.com>
2024-10-21 22:47:41 +0300

    Проходит все тесты

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,7 +1,41 @@
-#include <assert.h>
+ #include <assert.h>
 #include "str2int.h"
 #include "stdio.h"
+#include <limits.h>
 
 int str2int(const char *str) {
-    return 0;
+  int result = 0;
+  int sign = 1;
+
+  if (*str == '-') {
+    sign = -1;
+    str++;
+  } else if (*str == '+') {
+    str++;
+  }
+
+  if (*str == '\0') {
+    assert(0);
+  }
+
+  while (*str != '\0') {
+    if (*str < '0' || *str > '9') {
+      assert(0);
+    }
+
+    int digit = *str - '0';
+
+    if (sign == 1 && (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10))) {
+      assert(0);
+    }
+
+    if (sign == -1 && (-result < INT_MIN / 10 || (-result == INT_MIN / 10 && digit > -(INT_MIN % 10)))) {
+      assert(0);
+    }
+
+    result = result * 10 + digit;
+    str++;
+  }
+
+  return sign * result;
 }
