02377a1809ebdd6132e2a577e67efde5a258e0a9
Golubkov <chpokreacher@gmail.com>
2024-12-07 12:18:56 +0300

    Проходит все тесты

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,44 @@
-#include <assert.h>
 #include "int2str.h"
-#include "stdio.h"
+#include <stdlib.h>
 
 char* int2str(int number) {
-    char* str = "0";
-    return str;
+  int is_negative = 0;
+  unsigned int temp_number = number;
+
+  if (number < 0) {
+    is_negative = 1;
+    temp_number = -(unsigned int)number;  // Используем unsigned int для избежания переполнения
+  }
+
+  // Определяем длину строки
+  int length = 0;
+  unsigned int temp = temp_number;
+  while (temp > 0) {
+    length++;
+    temp /= 10;
+  }
+
+  if (is_negative || number == 0) {
+    length++;  // Добавляем место для знака минус или для нуля
+  }
+
+  // Выделяем память для строки
+  char* result = (char*)malloc(length + 1);
+
+  // Преобразуем число в строку
+  result[length] = '\0';
+  int i = length - 1;
+  while (temp_number > 0) {
+    result[i] = '0' + (temp_number % 10);
+    temp_number /= 10;
+    i--;
+  }
+
+  if (is_negative) {
+    result[0] = '-';
+  } else if (number == 0) {
+    result[0] = '0';
+  }
+
+  return result;
 }
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,7 +1,47 @@
 #include <assert.h>
+#include <stdlib.h>
+#include <limits.h>
 #include "str2int.h"
 #include "stdio.h"
 
 int str2int(const char *str) {
-    return 0;
+  assert(str != NULL);
+
+  // Проверка на пустую строку
+  if (*str == '\0') {
+    abort();
+  }
+
+  int sign = 1;
+  if (*str == '+' || *str == '-') {
+    if (*str == '-') {
+      sign = -1;
+    }
+    str++;
+    // Проверка на строку, содержащую только знак
+    if (*str == '\0') {
+      abort();
+    }
+  }
+
+  long long result = 0;
+  while (*str != '\0') {
+    int digit = *str - '0';
+    if (digit < 0 || digit > 9) {
+      abort();
+    }
+    if (result > (LONG_MAX - digit) / 10) {
+      abort();
+    }
+    result = result * 10 + digit;
+    str++;
+  }
+
+  result *= sign;
+
+  if (result > INT_MAX || result < INT_MIN) {
+    abort();
+  }
+
+  return (int)result;
 }
